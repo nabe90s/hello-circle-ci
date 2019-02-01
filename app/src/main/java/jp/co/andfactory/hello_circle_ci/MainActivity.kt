88cf2bc073cde4d6876470f31f3980f2fccef894
RyoWatanabe <30409123+wataanaber@users.noreply.github.com>
2019-02-01 14:36:25 +0900

    init

---
+++ app/src/main/java/jp/co/andfactory/hello_circle_ci/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.co.andfactory.hello_circle_ci
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
